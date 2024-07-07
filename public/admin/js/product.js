dd4716ba81c158a2af840d4585b94f13e57771fe
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-07-07 21:56:02 +0700

    Trang admin - Xoá vĩnh viễn một sản phẩm

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -20,3 +20,26 @@ if(buttonChangestatus.length > 0){
   });
 }
 // End Change one status
+
+// Delete Item
+const buttonDelete = document.querySelectorAll("[button-delete]");
+if(buttonDelete.length > 0) {
+  const formDeleteItem = document.querySelector("#form-delete-item");
+  const path = formDeleteItem.getAttribute("data-path");
+
+  buttonDelete.forEach(button => {
+    button.addEventListener("click", () => {
+      const isConfirm = confirm("Xóa sản phẩm này");
+
+      if(isConfirm){
+        const id = button.getAttribute("data-id");
+
+        const action = path + `/${id}?_method=DELETE`;
+        formDeleteItem.action = action; 
+  
+        formDeleteItem.submit(); 
+      }
+    });
+  });
+}
+// End Delete Item
