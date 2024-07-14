bc010257cb2bf2e97ceef89614b629a7bafa1ad6
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-07-14 22:00:46 +0700

    Bổ sung upload cho chỉnh sửa sản phẩm

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -28,7 +28,13 @@ router.post(
 
 router.get("/edit/:id", controller.edit);
 
-router.patch('/edit/:id', upload.single("thumbnail"), validate.createPost, controller.editPatch);
+router.patch(
+  '/edit/:id', 
+  upload.single("thumbnail"), 
+  uploadCloud.upload,
+  validate.createPost, 
+  controller.editPatch
+);
 
 router.get("/detail/:id", controller.detail);
 
