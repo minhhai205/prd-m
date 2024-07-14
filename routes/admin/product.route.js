978180d68caeba76a52f89e2eae780e9d5f59e4c
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-07-14 18:41:15 +0700

    Đẩy file tĩnh lên cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const multer = require('multer');
 const router = express.Router();
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 router.get('/', controller.index);
 
@@ -17,7 +18,13 @@ router.delete("/delete/:id", controller.deleteItem);
 
 router.get('/create', controller.create);
 
-router.post('/create', upload.single("thumbnail"), validate.createPost, controller.createPost);
+router.post(
+  '/create', 
+  upload.single("thumbnail"), 
+  uploadCloud.upload,
+  validate.createPost, 
+  controller.createPost
+);
 
 router.get("/edit/:id", controller.edit);
 
