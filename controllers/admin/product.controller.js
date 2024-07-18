ebad3d672cd7ddbcf0fa1a745700cc7b7f3ab4ae
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-07-19 02:30:09 +0700

    Thêm chọn danh mục sản phẩm - Phần thêm mới sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,10 +1,12 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 
 const systemConfig = require("../../config/system");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
+const createTreeHelper = require("../../helpers/createTree");
 // [GET] /admin/products
 module.exports.index = async(req, res) => {
 
@@ -127,8 +129,17 @@ module.exports.deleteItem = async(req, res) => {
 
 // [GET] /admin/products/create
 module.exports.create = async(req, res) => {
+  let find = {
+    deleted: false
+  };
+  
+  const records = await ProductCategory.find(find);
+
+  const newRecords = createTreeHelper.createTree(records);
+
   res.render("admin/pages/products/create", {
-    pageTitle : "Thêm mới sản phẩm"
+    pageTitle : "Thêm mới sản phẩm",
+    productsCategory: newRecords,
   });
 }
 
