29c07eaa98c919f90a91f4d0c5c34416bdc22fe4
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-07-23 17:34:28 +0700

    Làm trang sửa tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -59,3 +59,59 @@ module.exports.createPost = async(req, res) => {
   }
 
 }
+
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async(req, res) => {
+  try{
+    const find = {
+      deleted: false,
+      _id: req.params.id
+    }
+  
+    const account = await Account.findOne(find);
+    
+    const roles = await Role.find({
+      deleted: false
+    });
+    
+    res.render("admin/pages/accounts/edit", {
+      pageTitle : "Chỉnh sửa tài khoản",
+      account: account,
+      roles: roles,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/accounts`)
+  }
+}
+
+// // [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async(req, res) => {
+  const emailExit = await Account.findOne({
+    _id: { $ne: req.params.id}, 
+    email: req.body.email,
+    deleted: false
+  });
+  
+  if(emailExit){
+    req.flash("error", "Email đã tồn tại, vui lòng thử lại!");
+    res.redirect("back");
+  }
+  else{
+    if(req.body.password){
+      req.body.password = md5(req.body.password);
+    } 
+    else{
+      delete req.body.password;
+    }
+    
+    try {
+      await Account.updateOne({_id: req.params.id}, req.body);
+      req.flash("success", "Cập nhập tài khoản thành công!");
+    } catch (error) {
+      req.flash("error", "Cập nhập tài khoản thất bại!");
+    }
+  
+    res.redirect("back");
+  }
+  
+}
