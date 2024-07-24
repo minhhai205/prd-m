8ca70fa2e2c04c705db8248fbe083b20832d6101
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-07-24 23:12:34 +0700

    update

--- routes/admin/auth.route.js
+++ routes/admin/auth.route.js
@@ -6,7 +6,7 @@ const validate = require("../../validates/admin/auth.validate");
 
 router.get('/login', controller.login);
 
-router.post('/login', controller.loginPost);
+router.post('/login', validate.loginPost, controller.loginPost);
 
 router.get('/logout', controller.logout);
 
