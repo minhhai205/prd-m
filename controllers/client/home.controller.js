fa9b58cab77ac7774369901e49e2b75fd09ce35a
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-07-29 17:43:14 +0700

    Hiển thị danh sách sản phẩm nổi bật bên trang chủ client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,18 @@
+const Product = require("../../models/product.model");
 // [GET] /
-module.exports.index = (req, res) => {
+module.exports.index = async(req, res) => {
+  const productsFeatured = await Product.find({
+    deleted: false,
+    status: "active",
+    featured: "1",
+  }).limit(6);
+
+  productsFeatured.forEach(product => {
+    product.newPrice = ((1 - product.discountPercentage / 100) * product.price).toFixed(0);
+  });
+
   res.render("client/pages/home/index", {
-    pageTitle : "Trang chủ"
+    pageTitle : "Trang chủ",
+    productsFeatured: productsFeatured,
   });
 }
