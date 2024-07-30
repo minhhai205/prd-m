3264182b9edb2e18e02193e82e6eadf3d74a0b36
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-07-30 14:55:42 +0700

    Trang kết quả tìm kiếm bên client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,13 +1,14 @@
 
 const productRoutes = require("./products.route")
 const homeRoutes = require("./home.route")
+const searchRoutes = require("./search.route")
 const middlwerreCategory = require("../../middlewares/client/catagory.middleware")
 module.exports = (app) => {
   app.use(middlwerreCategory.category);
 
   app.use("/", homeRoutes)
-  // app.get("/", (req, res) =>{
-  //   res.render("client/pages/home/index")
-  // })
+  
   app.use("/products", productRoutes)
+
+  app.use("/search", searchRoutes)
 }
