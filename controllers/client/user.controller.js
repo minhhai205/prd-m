864212dfe1a28c228ffa30dbde7a758af2b152a3
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-08-07 16:35:37 +0700

    Lưu thêm user_id vào collection carts

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -81,6 +81,7 @@ module.exports.loginPost = async(req, res) => {
 // [GET] /user/logout
 module.exports.logout = async(req, res) => {
   res.clearCookie("tokenUser");
+  res.clearCookie("cartId");
   res.redirect("/");
 }
 
--- middlewares/client/user.middleware.js
+++ middlewares/client/user.middleware.js
@@ -1,4 +1,5 @@
 const User = require("../../models/user.model");
+const Cart = require("../../models/cart.model");
 
 module.exports.userInfo = async (req, res, next) => {
   if(req.cookies.tokenUser) {
@@ -10,6 +11,16 @@ module.exports.userInfo = async (req, res, next) => {
 
     if(user) {
       res.locals.user = user;
+      const cart = await Cart.findOne({user_id : user.id});
+
+      // Nếu user đã có card thì cập nhập lại cardId local
+      if(cart){
+        res.cookie("cartId", cart.id, {expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 100)}); 
+      }
+      // Không thì xét card hiện tại cho user
+      else{
+        await Cart.updateOne({_id : req.cookies.cartId}, {user_id : user.id});
+      }
     }
   }
 
